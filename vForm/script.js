0de9bfe9058702737882b0e23ff279c9b2de15b5
TorinBlackheart <eric@jlovell.com>
2023-06-03 18:48:40 -0700

    Final Upload of Artifact 4

--- vForm/script.js
+++ vForm/script.js
@@ -1,46 +1,144 @@
 function validateForm(){
-    
+  document.getElementById("errorMessages").innerHTML = "";
+  var isFNValid = validateNameField("FirstName", 20);
+  var isLNValid = validateNameField("LastName", 50);
+  var isEmailValid = validateEmail();
+  var isPhoneValid = validatePhoneField();
+  var isUsernameValid = validateNameField("Username", 12);
+  var isPasswordValid = validateNameField("Password", 7);
+  var isAddressValid = validateAddressField("Address");
+  var isCityValid = validateAddressField("City");
+  var isStateValid = validateDropDown("State");
+  var isCountryValid = validateDropDown("Country");
+  var isZipCodeValid = validateZipCode();
+  return (isFNValid && isLNValid && isEmailValid && isPhoneValid && isUsernameValid && isPasswordValid && isAddressValid && isCityValid && isStateValid && isCountryValid && isZipCodeValid);
 }
-function validateFNForm(){
-    //1) Create variable
-var validFirstname=false;
-//2) read value from HTML
-var firstname = document.getElementById("firstname").value;
-//3) Do validation
-if (firstname==="null" || firstname==="" || firstname.length > 20)
-    errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
-else
-   validFirstname = true;
-//4) Send error message to HTML
-document.getElementById("errorMessages").innerHTML = errorMessages;
 
-//5) return status of each field
-return (validFirstname);
-    }
-function validateLNForm(){
-        //1) Create variable
-var validLastname=false;
-//2) read value from HTML
-var Lastname = document.getElementById("lastname").value;
-//3) Do validation
-if (lastname==="null" || lastname==="" || lastname.length > 50)
-    errorMessages += "<p>The lastname is required and cannot be greater than 20 characters</p>";
-else
-   validlastname = true;
-//4) Send error message to HTML
-document.getElementById("errorMessages").innerHTML = errorMessages;
+function validateNameField(fieldName, maxLength) {
+  var validationError = document.getElementById("errorMessages").innerHTML;
+  //1) Create variable
+  var isValid = false;
 
-//5) return status of each field
-return (validlastname);
+  //2) read value from HTML
+  var fieldValue = document.getElementById(fieldName).value;
+
+  //3) Do validation
+  if (fieldValue === "null" || fieldValue === "" || fieldValue.length > maxLength)
+    validationError += "<p>The " + fieldName + " is required and cannot be greater than "+ maxLength +" characters</p>";
+  else
+    isValid = true;
+
+  //4) Send error message to HTML
+  document.getElementById("errorMessages").innerHTML = validationError;
+
+  //5) return status of each field
+  return (isValid);
 }
+
+function validatePhoneField() {
+  var validationError = document.getElementById("errorMessages").innerHTML;
+  //1) Create variable
+  var isValid = false;
+  var validCharacters = /^[0-9]+$/;
+  //2) read value from HTML
+  var fieldValue = document.getElementById("Phone").value;
+
+  //3) Do validation
+  if (fieldValue === null || fieldValue === "" || fieldValue.length > 15 || !fieldValue.match(validCharacters) )
+    validationError += "<p>Invalid phone number</p>";
+  else
+    isValid = true;
+
+  //4) Send error message to HTML
+  document.getElementById("errorMessages").innerHTML = validationError;
+
+  //5) return status of each field
+  return (isValid);
+}
+
 function validateEmail(){
-    var userEmail = document.getElementById("email").value;
-    var atpos = userEmail.indexOf("@");
-    var dotpos = userEmail.lastIndexOf(".");
-    if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
-        errorMessages = "<p>Invalid email</p>";
-    }
-    else{
-        return true;
-    }
+  var validationError = document.getElementById("errorMessages").innerHTML;
+  //1) Create variable
+  var isValid = false;
+  var validCharacters = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/;
+  //2) read value from HTML
+  var fieldValue = document.getElementById("Email").value;
+
+  //3) Do validation
+  if (fieldValue === null || fieldValue === "" || !fieldValue.match(validCharacters) )
+    validationError += "<p>Invalid Email</p>";
+  else
+    isValid = true;
+
+  //4) Send error message to HTML
+  document.getElementById("errorMessages").innerHTML = validationError;
+
+  //5) return status of each field
+  return (isValid);
+}
+
+function validateAddressField(fieldName){
+  var validationError = document.getElementById("errorMessages").innerHTML;
+  //1) Create variable
+  var isValid = false;
+
+  //2) read value from HTML
+  var fieldValue = document.getElementById(fieldName).value;
+
+  //3) Do validation
+  if (fieldValue === "null" || fieldValue === "")
+    validationError += "<p>The " + fieldName + " is required</p>";
+  else
+    isValid = true;
+
+  //4) Send error message to HTML
+  document.getElementById("errorMessages").innerHTML = validationError;
+
+  //5) return status of each field
+  return (isValid);
+
+}
+
+function validateDropDown(fieldName){
+  var validationError = document.getElementById("errorMessages").innerHTML;
+  //1) Create variable
+  var isValid = false;
+
+  //2) read value from HTML
+  var fieldValue = document.getElementById(fieldName).value;
+
+  //3) Do validation
+  if (fieldValue === "000")
+    validationError += "<p>You must select a " + fieldName +"</p>";
+  else
+    isValid = true;
+
+  //4) Send error message to HTML
+  document.getElementById("errorMessages").innerHTML = validationError;
+
+  //5) return status of each field
+  return (isValid);
+
+}
+function validateZipCode(){
+  var validationError = document.getElementById("errorMessages").innerHTML;
+  //1) Create variable
+  var isValid = false;
+  var country = document.getElementById("Country").value;
+  var validCharacters = /^[0-9]+$/;
+  //2) read value from HTML
+  var fieldValue = document.getElementById("ZipCode").value;
+
+  //3) Do validation
+  if (country === "USA" && (fieldValue === "null" || fieldValue.length === 0 || fieldValue.length > 5 || !fieldValue.match(validCharacters)))
+    validationError += "<p>You must provide a valid ZipCode that is between 1-5 digits</p>";
+  else
+    isValid = true;
+
+  //4) Send error message to HTML
+  document.getElementById("errorMessages").innerHTML = validationError;
+
+  //5) return status of each field
+  return (isValid);
+
 }
